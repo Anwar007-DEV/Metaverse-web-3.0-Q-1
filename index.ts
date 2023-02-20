34054e5a46b822d55ae6168b0769adbd7e4d5dd9
Anwar ul hassan Nawaz <65052807+Anwar007-DEV@users.noreply.github.com>
2023-02-20 16:34:23 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,34 @@
+// TASK # 1
+// console.log ("Developing a new universe:");
+
+// TASK # 2
+// let a = 10;
+// let b = 2;
+// let ans = a%b;
+// console.log (ans);
+
+
+// TASK # 3
+// console.log( "we are adding two numbers:")
+// console.log ("11 + 2") 
+// console.log ("answer is:")
+// const number1 = 11
+// const number2 = 2
+// const sumition = number1 + number2 
+// console.log (sumition)
+
+
+
+// TASK # 4
+// let multiplication;
+// let firstdigit = 9
+// let scnddigit = 2
+// multiplication = firstdigit * scnddigit;
+// console.log(multiplication);
+
+
+
+
+
+
+
